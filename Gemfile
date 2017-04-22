5b90659424fea63918b8839d8b861bee0c5c6610
Warner Nielsen <bwn888@gmail.com>
2017-04-21 22:21:52 -0600

    Setup AWS and edit production.rb

--- Gemfile
+++ Gemfile
@@ -17,6 +17,9 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.5'
+gem 'paperclip'
+gem 'aws-sdk', '~> 2.3'
+gem 'dotenv'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,14 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('warner-profile-app'),
+      access_key_id: ENV.fetch()
+    }
+  }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
